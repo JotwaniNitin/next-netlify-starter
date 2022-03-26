ae878b6ccd0694e562b494511375ea3324779feb
JotwaniNitin <32052852+JotwaniNitin@users.noreply.github.com>
2022-03-26 15:55:43 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Having fun with the Crio Session!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Thanks Sridher Jeyachandran!
         </p>
       </main>
 
